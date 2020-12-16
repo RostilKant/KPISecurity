282b88620bdccdd400245d38619b6dfb4b83d6db
RostilKant <rostilnytskiy@gmail.com>
2020-12-16 23:38:52 +0200

    initial create

---
+++ Lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿ddddusing System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
